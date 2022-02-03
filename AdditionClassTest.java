b1caf48af009e3b73ee76e722b3b923271e81078
daniel-lee-user <danielhlee02@gmail.com>
2022-02-03 00:07:30 -0800

    Fixed test errors

--- AdditionClassTest.java
+++ AdditionClassTest.java
@@ -4,9 +4,15 @@ public class AdditionClassTest {
     // need to think of tests and import junit libraries
     @Test
     public void testAdd() {
-        assertEquals(5, AdditionClass.add(1,3));
+        assertEquals(5, AdditionClass.add(1,4));
         // This test should fail because 5 != 4
     }
     // 1 test 1 failure
-    // time to push
+    // time to commit and push
+    // logon to ssh
+    // clone repo
+    // fails again
+    // time to fix code
+    // running it locally - passes
+    // commit, push and run it on ssh
 }
